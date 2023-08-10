89f493fd0f17f5f9549e63dd4a6824e479c27f66
amirn003 <amir.naar@gmail.com>
2023-08-10 15:01:30 +0200

    add lists route, controller, views

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ActionController::Base
+  before_action :find_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_set)
+    if @list.save
+      redirect_to list_path(@list) #lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def params_set
+    params.require(:list).permit(:name)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+end
