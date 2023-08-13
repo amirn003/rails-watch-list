aeededcb3c504ba4ef0dedf30a5a4c3c2a9a48ba
amirn003 <amir.naar@gmail.com>
2023-08-14 00:16:51 +0200

    update cards, show view

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,16 +4,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   updateNavbar() {
-    if (window.scrollY >= 100) {
+    if (window.scrollY >= 175) {
       this.element.classList.add("navbar-lewagon-white")
-      console.log("SUPERIOR")
-      console.log(window.scrollY)
-      console.log("WINDOW")
-      console.log(window.innerHeight)
     } else {
       this.element.classList.remove("navbar-lewagon-white")
-      console.log("INFERIOR")
-      console.log(window.scrollY)
     }
   }
 }
