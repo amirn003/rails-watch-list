2bb26f7da99af1ebc43cb262d8867ec2c530091b
amirn003 <amir.naar@gmail.com>
2023-08-17 19:41:53 +0200

    remove asset config

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module RailsWatchList
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.enabled = true
 
     # Don't generate system test files.
     config.generators.system_tests = nil
